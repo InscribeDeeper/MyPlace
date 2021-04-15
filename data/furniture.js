1b4cf7f0700a04725f832603f36f0aae4f58720f
WY <hey.weiyang@gmail.com>
2021-04-14 22:06:56 -0400

    update folder structure

---
+++ data/furniture.js
@@ -0,0 +1,16 @@
+
+Furniture = {
+    "_id": "furniture_id1",
+    "user_id": "user_collection_id_1", 
+    "comment_id": ["comment_collection_id_1232", "comment_collection_id_1234",],
+    "category": ["electronics", "computer"], // refer amazon
+    "location": "1253 Garden St", // google map 
+    "price": 10, 
+    "description": "long paragraph string", // used age 4/10  // search by descrption match
+    "photos": ["binary_file1", "binary_file2"],
+    "like": 20,
+    "dislike": 4,
+    "purchase_link": "https://www.amazon.com/gp/product/B07TZSCDJD/ref=ppx_yo_dt_b_asin_image_o00_s00?ie=UTF8&psc=1",
+    "sold": FALSE,
+    "contact": "vx_abcd / 155 564 1235"
+}
