bd8247462e995295ace406156536d7dcf5a720c8
WY <hey.weiyang@gmail.com>
2021-04-14 22:04:15 -0400

    update folder structure

---
+++ data/comments.js
@@ -0,0 +1,12 @@
+// wjq
+
+
+Comment = {
+    "_id": "comment_collection_id_1",
+    "user_id": "user_collection_id_1",
+    "comment": "this is the comment for xxx",
+    "report_count": 5,
+    "helpful_count": 24, // refer amazon
+}
+ 
+
