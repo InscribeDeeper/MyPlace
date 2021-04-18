652433c567c20218a0d2d355633f42b64ee02a3f
Yixuan Wang <ywang385@stevens.edu>
2021-04-18 02:29:31 -0400

    change

--- data/rental.js
+++ data/rental.js
@@ -33,31 +33,38 @@ const ExportedMethos = {
         const newId = insertInfo.insertedId
         await users.addRentalToUser(bookId, newId, user, location, price, bedroom, bathroom, space, description, photos, utility, like, dislike, labels, contact)
         return await this.getRentalById(newId)
-    }
+    },
 
+
+    //Read ('get all', 'get by id')
+
+    async getAllRental(){
+        const rentalCollection = await rental()
+        const rentalList = await rentalCollection.find({}).toArray()
+        if(!rentalList) throw 'Error: no rental in the system'
+        return rentalList
+    },
+
+    
 
 }
 
+// DB Example
+// rental = {
+//     "_id": "rental_id1",
+//     "user_id": "user_collection_id_1",
+//     "comment_id": ["comment_collection_id_101","comment_collection_id_102"],
+//     "location": "123 Washington St", // google map 
+//     "price": 1000,
+//     "bedroom": 2,
+//     "bathroom": 2,
+//     "space": 85, // square feet unit should be unify
+//     "description": "long paragraph string",
+//     "photos": ["binary_file1", "binary_file2"],
+//     "utility": "string",
+//     "like": 20,
+//     "dislike": 4,
+//     "labels": ["warm", "big", "cheap", "luxury", "convenient", ""],
+//     "contact": "vx_abcd / 155 564 1235"
+// }
 
-rental = {
-    "_id": "rental_id1",
-    "user_id": "user_collection_id_1",
-    "comment_id": ["comment_collection_id_101","comment_collection_id_102"],
-    "location": "123 Washington St", // google map 
-    "price": 1000,
-    "bedroom": 2,
-    "bathroom": 2,
-    "space": 85, // square feet unit should be unify
-    "description": "long paragraph string",
-    "photos": ["binary_file1", "binary_file2"],
-    "utility": "string",
-    "like": 20,
-    "dislike": 4,
-    "labels": ["warm", "big", "cheap", "luxury", "convenient", ""],
-    "contact": "vx_abcd / 155 564 1235"
-}
-
-
-//lalalala
-
-//yixuan branch
