6eb0e3a6f289c9d6ec9a47625747e1b6cae53a7d
Shaobo Li <sli141@stevens.edu>
2021-04-18 02:25:40 -0400

    "4/17 2"

---
+++ tasks/seeds.js
@@ -0,0 +1,18 @@
+const dbConnection = require('../config/mongoConnection');
+const data = require('../data/');
+const furniture = data.furniture;
+
+const main = async () => {
+  const db = await dbConnection();
+  await db.dropDatabase();
+/** start of the testing code */ 
+// function Create(category:[], location, price, description, photos:[], purchase_link, sold, contact) 没有customer_id，like和dislike
+const f1 = await furniture.create(["f","u"], "a", 20, "good",[],"b", false, 123)
+console.log(f1);
+
+/** End of the testing code */  
+  console.log('Done seeding database');
+  await db.serverConfig.close();
+};
+
+main().catch(console.log);
