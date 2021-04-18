7485bc4836ca01a205e04f77dc21973c0a9ad9b1
Yixuan Wang <ywang385@stevens.edu>
2021-04-18 02:24:47 -0400

    change by yixuan

--- data/users.js
+++ data/users.js
@@ -5,4 +5,6 @@
 //在你这里面记得加一个addRentalToUser和removeRentalToUser的函数
 //我发现我们这个project还是得用sub-document比较
 //我的里面导进你的函数叫 users.addRentalToUser(bookId, newId, user, location, price, bedroom, bathroom, space, description, photos, utility, like, dislike, labels, contact) 和 users.removeRentalToUser
+
+//主要是如果要用relational的话...我不会！
 //from wyx
