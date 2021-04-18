9fdfd7085c4337f2889df3a0926ae6142cfdc3e5
Shaobo Li <sli141@stevens.edu>
2021-04-18 02:05:42 -0400

    4/17

--- data/furniture.js
+++ data/furniture.js
@@ -14,3 +14,66 @@
 //     "sold": FALSE,
 //     "contact": "vx_abcd / 155 564 1235"
 // }
+
+const mongoCollections = require('../config/mongoCollections');
+const furniture = mongoCollections.furniture;
+const uuid = require('uuid');
+
+async function getByFurnitureId(id){
+     
+     const furnitureCollection = await furniture();
+     const theFurniture = await furnitureCollection.findOne({_id:id});
+
+     if(!theFurniture) throw 'the book is not found in books data';
+     return theFurniture;
+}
+
+async function Create(user_id, comment_id, category, location, price, description, photos, likes, dislike, purchase_link, sold, contact){
+     // check for title
+     // if(!title) throw 'title is not defined.';
+     // if(typeof title != 'string') throw `${title} is not a string.`;
+     
+     // if(!authorFirstName) throw 'authorFirstName is not defined.';
+     // if(typeof authorFirstName != 'string') throw `${authorFirstName} is not a string`;
+
+     // if(!authorLastName) throw 'authorLastName is not defined.';
+     // if(typeof authorLastName!= 'string') throw `${authorLastName} is not a string`;
+
+     // if(!genre) throw 'genre is not defined.';
+     // if(!Array.isArray(genre)) throw 'genre shall be an array.';
+     // for(let x of genre)
+     //      if(typeof x != 'string') throw `all elements in genere must be a string. ${x} is not a string`;
+
+     // if(!datePublished) throw 'publishing date is not defined.';
+     // if(!datePublished.match(/^\d{1,2}\/\d{1,2}\/\d{4}$/g)) throw 'the date is invalid';
+
+     // if(!summary) throw 'summary is not defined.';
+     // if(typeof summary != 'string') throw `${summary} is not a string`;
+
+     // if(title.trim().length == 0 || authorFirstName.trim().length == 0 || authorLastName.trim().length == 0 || genre.length == 0 || summary.trim().length == 0)
+     //      throw 'parameters can not be empty';
+
+     // add to collection
+     const furnitureCollection = await furniture();
+
+     let newBook = {
+          _id:uuid.v4(),
+          comment_id:[], 
+          category:[], 
+          location, 
+          price, 
+          description, 
+          photos:[], 
+          likes, 
+          dislike, 
+          purchase_link, 
+          sold, 
+          contact   
+     }
+     // console.log(typeof newBook._id + "book")
+     // console.log(newBook._id + "book") 
+     const newInsertedBook = await booksCollection.insertOne(newBook);
+     if (newInsertedBook.insertedCount === 0) throw 'Insert failed!';
+     // console.log(typeof newInsertedBook.insertedId + " book create")
+     return await this.getByBookId(newInsertedBook.insertedId);
+}
