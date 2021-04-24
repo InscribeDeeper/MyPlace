21b03db8d9e53a7b6635101e6d1ad7d4e5356e35
Jiaqing Wang <jwang198@stevens.edu>
2021-04-24 13:19:46 -0400

    Update DB comments

--- data/comments.js
+++ data/comments.js
@@ -1,11 +1,58 @@
-// wjq
-//Jiaqing Confirm
+//Jiaqing Wang
 
 
-Comment = {
-    "_id": "comment_collection_id_1",
-    "user_id": "user_collection_id_1",
-    "comment": "this is the comment for xxx",
-    "report_count": 5,
-    "helpful_count": 24, // refer amazon
+// Comment = {
+//     "_id": "comment_collection_id_1",
+//     "user_id": "user_collection_id_1",
+//     "comment": "this is the comment for xxx",
+//     "report_count": 5,
+//     "helpful_count": 24, // refer amazon
+// }
+
+const mongoCollections = require('../config/mongoCollections');
+const comments = mongoCollections.comments;
+const uuid = require('uuid');
+
+
+async function getByCommentId(id) {
+
+    const commentsCollection = await comments();
+    const theComments = await commentsCollection.findOne({ _id: id });
+
+    if (!theComments) throw 'This comment has been removed by Admin';
+    return theComments;
+}
+
+
+async function addComments(user_id, comment, report_count, helpful_count) {
+    if (!user_id || !comment || !report_count || !helpful_count)
+        throw 'Error, insert comment missing one or more fields'
+
+    if (typeof(user_id) != String)
+        throw 'Error, the datatype of [user_id] is supposed to be STRING'
+
+    if (typeof(report_count) != Number)
+        throw 'Error, the datatype of field [report_count] is supposed to be NUMBER'
+
+    if (typeof(helpful_count) != Number)
+        throw 'Error, the datatype of field [helpful_count] is supposed to be NUMBER'
+
+    const commentsCollection = await comments
+
+    const newComments = {
+        user_id: user_id,
+        comment: comment,
+        report_count: report_count,
+        helpful_count: helpful_count,
+        _id: uuid.v4()
+    }
+
+    const newInsertedComments = await commentsCollection.insertOne(newComments);
+    if (newInsertedComments.insertedCount === 0) throw 'No Comment(s) input';
+    return await this.getByCommentsId(newInsertedComments.insertedId);
+}
+
+module.exports = {
+    getByCommentId,
+    addComments
 }
