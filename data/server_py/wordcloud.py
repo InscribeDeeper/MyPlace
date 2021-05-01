1b02e948d931045c2eaa0810d4b3c3d2cb5eb7d8
WY <hey.weiyang@gmail.com>
2021-05-01 01:27:46 -0400

    Update pytest on server side

--- data/server_py/wordcloud.py
+++ data/server_py/wordcloud.py
@@ -1,5 +1,16 @@
+#encode:utf-8
 # ?? 可以弄 comments的 wordcloud吗
 
 # 如果可以, 要怎样才能输出?
 #  返回高频词 很容易
 # 但是要怎样可视化 是不是有什么工具
+
+# 这里要写好对应的命令调用工具, 
+# 即先用python文件写好 命令行调用的方法, 然后在联系这里
+
+
+
+import sys
+for i in range(len(sys.argv)):
+    print('arg'+str(i),sys.argv[i])
+
--- tasks/seeds.js
+++ tasks/seeds.js
@@ -83,3 +83,6 @@ async function main() {
 }
 
 main().catch(console.log);
+
+
+
