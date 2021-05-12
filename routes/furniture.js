4c67db6134c9d00cf99fe9a69cab321e2a3ef288
Shaobo Li <sli141@stevens.edu>
2021-05-12 17:12:38 -0400

    kind of finishing furniture routes

--- routes/furniture.js
+++ routes/furniture.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const data = require('../data');
 const furnitureData = data.furniture;
 const userData = data.users;
-// const commentData = data.comments;
+const commentData = data.comments;
 const verify = require('../data/verify');
 const xss = require('xss');
 
@@ -100,17 +100,17 @@ router.get('/:id', async (req, res) => {
     let id = req.params.id.trim();
     let errors = [];
     if (!verify.validString(id)) {
-        errors.push('Id of the restaurant must be provided')
+        errors.push('Id of the furniture must be provided')
         return res.render('errors/error', {
             title: 'Errors',
-            partial: 'errors-script',
+            // partial: 'errors-script',
             errors: errors
         });
     }
     
     try {
-        const restaurant = await restaurantData.getRestaurantById(id);
-        const allReviews = await reviewData.getAllReviewsOfRestaurant(id);
+        const furniture = await furnitureData.getFurnitureById(id);
+        const allComments = await commentData.getAllComments(id);
         
         // Reviews will be a list of objects s.t. each object will hold all the info for a single review
         /*
@@ -122,72 +122,74 @@ router.get('/:id', async (req, res) => {
                 comments: []
             }
         */
-        const reviews = [];
-        for (const review of allReviews) {
+        const comments = [];
+        for (let eachCommment of allComments) {
             let current = {};
-            let { firstName, lastName, age } = await userData.getUserById(review.reviewerId);
-            current.id = review._id;
-            current.name = firstName + ' ' + lastName;
-            current.age = age;
-            current.text = review.reviewText;
-            current.metrics = review.metrics;
-            current.likes = review.likes;
-            current.dislikes = review.dislikes;
-            current.reported = review.reported;
 
-            let allComments = await commentData.getAllCommentsOfReview(review._id);
-            let comments = [];
-            for (const comment of allComments) {
-                let currentComment = {};
-                let {firstName, lastName, age} = await userData.getUserById(comment.userId);
-                currentComment.name = firstName + ' ' + lastName;
-                currentComment.age = age;
-                currentComment.text = comment.text
-                comments.push(currentComment);
+            let commentedUser = await userData.getUserById(eachCommment._id); // get each comment's owner
+            // current.id = review._id;
+            current.name = commentedUser.first_name + ' ' + commentedUser.last_name;
+            current.age = commentedUser.age;
+            // current.text = eachCommment.comment;
+            // current.metrics = review.metrics;
+            // current.likes = review.likes;
+            // current.dislikes = review.dislikes;
+            // current.reported = review.reported;
+
+            // let allComments = await commentData.getAllCommentsOfReview(review._id);
+            // let comments = [];
+            // for (const comment of allComments) {
+            //     let currentComment = {};
+            //     let {firstName, lastName, age} = await userData.getUserById(comment.userId);
+            //     currentComment.name = firstName + ' ' + lastName;
+            //     currentComment.age = age;
+            //     currentComment.text = comment.text
+            //     comments.push(currentComment);
             }
-            current.comments = comments;
+            current.comments = comment;
 
-            let max = 5;
-            current.filledStars = verify.generateList(current.metrics.rating);
-            current.unfilledStars = verify.generateList(max - current.metrics.rating);
-            current.filledDollars = verify.generateList(current.metrics.price)
-            reviews.push(current);
-        }
+            // let max = 5;
+            // current.filledStars = verify.generateList(current.metrics.rating);
+            // current.unfilledStars = verify.generateList(max - current.metrics.rating);
+            // current.filledDollars = verify.generateList(current.metrics.price)
+            // reviews.push(current);
+        // }
 
         // Calculate percentages for ratings based off of reviews
-        const numReviews = allReviews.length;
-        restaurant.rating = (restaurant.rating / numReviews).toFixed(2);
-        restaurant.price = (restaurant.price / numReviews).toFixed(2);
-        restaurant.distancedTables = ((restaurant.distancedTables / numReviews) * 100).toFixed(2);
-        restaurant.maskedEmployees = ((restaurant.maskedEmployees / numReviews) * 100).toFixed(2);
-        restaurant.noTouchPayment = ((restaurant.noTouchPayment / numReviews) * 100).toFixed(2);
-        restaurant.outdoorSeating = ((restaurant.outdoorSeating / numReviews) * 100).toFixed(2);
+        // const numReviews = allReviews.length;
+        // restaurant.rating = (restaurant.rating / numReviews).toFixed(2);
+        // restaurant.price = (restaurant.price / numReviews).toFixed(2);
+        // restaurant.distancedTables = ((restaurant.distancedTables / numReviews) * 100).toFixed(2);
+        // restaurant.maskedEmployees = ((restaurant.maskedEmployees / numReviews) * 100).toFixed(2);
+        // restaurant.noTouchPayment = ((restaurant.noTouchPayment / numReviews) * 100).toFixed(2);
+        // restaurant.outdoorSeating = ((restaurant.outdoorSeating / numReviews) * 100).toFixed(2);
 
         // Get the restaurant's photos from calling Yelp API
-        const client = yelp.client(apiKey);
-        const matchRequest = {
-            name: restaurant.name,
-            address1: restaurant.address,
-            city: 'Hoboken',
-            state: 'NJ',
-            country: 'US'
-        };
+        // const client = yelp.client(apiKey);
+        // const matchRequest = {
+        //     name: restaurant.name,
+        //     address1: restaurant.address,
+        //     city: 'Hoboken',
+        //     state: 'NJ',
+        //     country: 'US'
+        // };
 
-        const matchRes = await client.businessMatch(matchRequest);
-        const jsonRes = matchRes.jsonBody;
-        let results = jsonRes.businesses;
-        let result = results[0];
-        let photos = [];
-        if (results.length > 0) {
-            const businessRes = await client.business(result.id);
-            const jsonRes2 = businessRes.jsonBody;
-            photos = jsonRes2.photos;
-        }
+        // const matchRes = await client.businessMatch(matchRequest);
+        // const jsonRes = matchRes.jsonBody;
+        // let results = jsonRes.businesses;
+        // let result = results[0];
+        // let photos = [];
+        // if (results.length > 0) {
+        //     const businessRes = await client.business(result.id);
+        //     const jsonRes2 = businessRes.jsonBody;
+        //     photos = jsonRes2.photos;
+        // }
 
-        res.render('restaurants/single', {
-            partial: 'restaurants-single-script',
-            title: `${restaurant.name}`,
-            authenticated: req.session.user ? true : false,
+        res.render('furnitures/single', {
+            // partial: 'restaurants-single-script',
+            title: "Furniture",
+            // authenticated: req.session.user ? true : false,
+            authenticated: true,
             user: req.session.user,
             restaurant: restaurant,
             reviews: reviews,
@@ -197,7 +199,7 @@ router.get('/:id', async (req, res) => {
         errors.push(e);
         res.status(500).render('errors/error', {
             title: 'Errors',
-            partial: 'errors-script',
+            // partial: 'errors-script',
             errors: errors
         });
     }
