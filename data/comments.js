308b1386d5411b76b38f39d9dc91087f1b13514d
Jiaqing Wang <jwang198@stevens.edu>
2021-05-13 22:27:45 -0400

    更改updateComment部分，点赞功能待加入

--- data/comments.js
+++ data/comments.js
@@ -16,7 +16,7 @@ async function getCommentById(id) {
 }
 
 
-async function addComments(user_id, comment, report_count, helpful_count) {
+async function addComments(user_id, comment) {
     //用户是不是不应该有权限input report_count 和 helpful_count，是不是应该只保留前两个参数,
     //如果存储的是userid的话，如何实现读取当前登录用户的id
     if (!verifier.validString(comment)) throw 'Invalid Comment'
@@ -25,11 +25,13 @@ async function addComments(user_id, comment, report_count, helpful_count) {
 
     const commentsCollection = await comments()
 
+    const initialUserIdList = []
+
     const newComments = {
         user_id: user_id,
         comment: comment,
-        report_count: report_count, //这里是不是要存id?
-        helpful_count: helpful_count,
+        reports: initialUserIdList, //存userId，但是开始的时候是不是这俩都是空的应该
+        helpful: initialUserIdList,
         _id: uuid.v4()
     }
 
@@ -62,15 +64,17 @@ async function deleteComment(id) {
 }
 
 
-async function updateComment(commentId, updatedInfo) {
+async function updateComment(commentId, updatedInfo) { //不包括点赞点踩的功能
     if (!verifier.validString(commentId)) throw "User id is not a valid string.";
 
     //Unfinished, should helpful_count and helpful_count store a count of likes or userid?
     const commentCollection = await comment();
+
+    const theComment = getCommentById(commentId)
     let updatingInfo = {
         comment: updatedInfo.comment,
-        report_count: updatedInfo.report_count, //id?
-        helpful_count: updatedInfo.helpful_count //id?
+        report_count: theComment.reports,
+        helpful_count: theComment.helpful
     }
     const updatedComment = await commentCollection.updateOne({ _id: id }, { $set: updatingInfo })
     if (!updatedComment.matchedCount && !updatedComment.motifiedCount) throw 'updated failed.'
