4a2895fe9f78bca143ef4afbcca93fe92e40bf69
Yixuan Wang <ywang385@stevens.edu>
2021-05-15 04:25:31 -0400

    update

--- data/rental.js
+++ data/rental.js
@@ -11,13 +11,15 @@ const ExportedMethods = {
 
     //CREATE
    
-    async createRental(location, price, bedroom, bathroom, space, description, photos, utility, like, dislike, labels, contact, longitute, latitute, userId) {
+    async createRental(location, price, bedroom, bathroom, space, description, photo_link, utility, like, dislike, labels, contact, longitute, latitute, userId) {
+
+        
 
         if (!verifier.validString(location)) throw "Location is not a valid string.";
-        if (!verifier.validNum(price)) throw "Price is not a valid number.";
-        if (!verifier.validNum(bedroom)) throw "Bedtoom is not a valid number.";
-        if (!verifier.validNum(bathroom)) throw "Bathroom is not a valid number.";
-        if (!verifier.validNum(space)) throw "Space is not a valid number.";
+        if (!verifier.validNum(parseInt(price))) throw "Price is not a valid number.";
+        if (!verifier.validNum(parseInt(bedroom))) throw "Bedtoom is not a valid number.";
+        if (!verifier.validNum(parseInt(bathroom))) throw "Bathroom is not a valid number.";
+        if (!verifier.validNum(parseInt(space))) throw "Space is not a valid number.";
         if (!verifier.validString(description)) throw "Description is not a valid string.";
         // if (!verifier.validString(photo)) throw "photo is not a valid string.";
         if (!verifier.validString(utility)) throw "utlity is not a valid string.";
@@ -28,21 +30,21 @@ const ExportedMethods = {
         const rentalCollection = await rental()
 
         const newRental = {
+            _id: uuid.v4(),
             location: location, 
             price: price, 
             bedroom: bedroom, 
             bathroom: bathroom, 
             space: space, 
             description: description , 
-            photos: photos, 
+            photo_link: photo_link, 
             utility: utility, 
             like: like, 
             dislike: dislike, 
             labels: labels, 
             contact: contact,
             longitute: longitute,
-            latitute: latitute,
-            _id: uuid.v4()
+            latitute: latitute
         }
         const insertInfo = await rentalCollection.insertOne(newRental)
         if (insertInfo.insertedCount === 0) throw 'Could not add a rental post';
