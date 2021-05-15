e3c2e70ef7cf78e0be5f116e4cd15bcedf71ff3b
WY <hey.weiyang@gmail.com>
2021-05-15 03:08:15 -0400

    update private

--- routes/private.js
+++ routes/private.js
@@ -15,69 +15,71 @@ router.get("/", async (req, res) => {
 	myUser = await userData.getUserByUserName(userName);
 	// myUser._id
 
-	//###########################
-	// My Reviews History
-	// this.name; this.type, this.text
-	//###########################
-	let reviewHistory = [];
-	const myComments = myUser.comments_id || [];
-	for (let eachCommmentID of myComments) {
-		eachCommment = await commentData.getCommentById(eachCommmentID);
+	// //###########################
+	// // My Reviews History
+	// // this.name; this.type, this.text
+	// //###########################
+	// let reviewHistory = [];
+	// const myComments = myUser.comments_id || [];
+	// for (let eachCommmentID of myComments) {
+	// 	eachCommment = await commentData.getCommentById(eachCommmentID);
 
-		current = {};
-		current_furniture_id = await commentData.getFurnitureIDbyCommentID(eachCommment._id);
-		current_rental_id = await commentData.getRentalIDbyCommentID(eachCommment._id);
+	// 	current = {};
+	// 	current_furniture_id = await commentData.getFurnitureIDbyCommentID(eachCommment._id);
+	// 	current_rental_id = await commentData.getRentalIDbyCommentID(eachCommment._id);
 
-		if (current_furniture_id) {
-			item_type = "Furniture";
-			current_furniture = await furnitureData.getFurnitureById(current_furniture_id);
-			item_name = current_furniture.name;
-			item_id = current_furniture._id;
-		} else {
-			item_type = "Rental";
-			current_rental = await rentalData.getRentalById(current_rental_id);
-			item_name = current_rental.name;
-			item_id = current_rental._id;
-		}
+	// 	if (current_furniture_id) {
+	// 		item_type = "Furniture";
+	// 		current_furniture = await furnitureData.getFurnitureById(current_furniture_id);
+	// 		item_name = current_furniture.name;
+	// 		item_id = current_furniture._id;
+	// 	} else {
+	// 		item_type = "Rental";
+	// 		current_rental = await rentalData.getRentalById(current_rental_id);
+	// 		item_name = current_rental.name;
+	// 		item_id = current_rental._id;
+	// 	}
 
-		current._id = item_id;
-		current.name = item_name;
-		current.type = item_type;
-		current.text = eachCommment.comment;
-		reviewHistory.push(current);
-	}
+	// 	current._id = item_id;
+	// 	current.name = item_name;
+	// 	current.type = item_type;
+	// 	current.text = eachCommment.comment;
+	// 	reviewHistory.push(current);
+	// }
 
-	//###########################
-	// My ownedFurnitures
-	// this.name; this.type, this.text
-	//###########################
-	let ownedFurnitures = [];
-	const myFurnitures = myUser.furniture_id || [];
-	for (let eachFurnitureID of myFurnitures) {
-        current = {};
-		current_furniture = await furnitureData.getFurnitureById(eachFurnitureID);
-        current.likes = current_furniture.likes
-        current.num_comments = current_furniture.comments_id.length
+	// //###########################
+	// // My ownedFurnitures
+	// // this.name; this.type, this.text
+	// //###########################
+	// let ownedFurnitures = [];
+	// const myFurnitures = myUser.furniture_id || [];
+	// for (let eachFurnitureID of myFurnitures) {
+    //     current = {};
+	// 	current_furniture = await furnitureData.getFurnitureById(eachFurnitureID);
+    //     current.likes = current_furniture.likes
+    //     current.num_comments = current_furniture.comments_id.length
 
-	}
-	//###########################
-	// My favoritedFurnitures
-	// this.name; this.type, this.text
-	//###########################
-	let favoritedFurnitures = [];
-	const ownedFurnituresIdList = myUser.furniture_id || [];
-	for (let eachCommmentID of myComments) {
-		eachCommment = await commentData.getCommentById(eachCommmentID);
+	// }
+	// //###########################
+	// // My favoritedFurnitures
+	// // this.name; this.type, this.text
+	// //###########################
+	// let favoritedFurnitures = [];
+	// const ownedFurnituresIdList = myUser.furniture_id || [];
+	// for (let eachCommmentID of myComments) {
+	// 	eachCommment = await commentData.getCommentById(eachCommmentID);
 
-		current = {};
-		current_furniture = await commentData.getFurnitureIDbyCommentID(eachCommment._id);
-		current_rental = await commentData.getRentalIDbyCommentID(eachCommment._id);
-		item = current_furniture || current_rental;
-		current.name = item.name;
-		current.type = current_furniture ? "Furniture" : "Rental";
-		current.text = eachCommment.comment;
-		reviewHistory.push(current);
-	}
+	// 	current = {};
+	// 	current_furniture = await commentData.getFurnitureIDbyCommentID(eachCommment._id);
+	// 	current_rental = await commentData.getRentalIDbyCommentID(eachCommment._id);
+	// 	item = current_furniture || current_rental;
+	// 	current.name = item.name;
+	// 	current.type = current_furniture ? "Furniture" : "Rental";
+	// 	current.text = eachCommment.comment;
+	// 	reviewHistory.push(current);
+	// }
+
+
 
 	return res.render("users/myProfile", {
 		authenticated: true,
