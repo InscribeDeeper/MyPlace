ce808fef6f9a8236d0cac4c44b2f124fb5525d37
Shaobo Li <sli141@stevens.edu>
2021-05-15 20:12:01 -0400

    sdaf

--- data/furniture.js
+++ data/furniture.js
@@ -17,8 +17,8 @@ async function createFurniture(userId, name, category, location, price, descript
     // add to collection
     if (!verifier.validString(userId)) throw "userId is not a valid user id.";
     if (!verifier.validString(name)) throw "userId is not a valid user id.";
-    if (!verifier.validString(category)) throw "category is not a valid category.";
-    if (!verifier.validString(location)) throw "Last name is not a valid string.";
+//     if (!verifier.validString(category)) throw "category is not a valid category.";
+//     if (!verifier.validString(location)) throw "Last name is not a valid string.";
 //     if (!parseFloat(price)) throw "Price is not a valid number.";
 //     if (!verifier.validString(description)) throw "userName is not a valid string.";
 //     if (!verifier.validString(photo)) throw "photo is not a valid string.";
